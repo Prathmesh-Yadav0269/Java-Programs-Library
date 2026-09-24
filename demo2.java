64b9a88d23e277dbae4219234f78bd1b91ba27af
Prathmesh-Yadav0269 <yadavprathmesh88180269@gmail.com>
2026-09-24 13:37:43 +0530

    Add second Java program

---
+++ demo2.java
@@ -0,0 +1,10 @@
+public class demo2{
+    public satic void main(String [] args)
+    {
+        
+        int a=2;
+        int b =4;
+        int sum =a+b;
+        System.out.println("The sum of a and b is: " + sum);
+    }
+}
